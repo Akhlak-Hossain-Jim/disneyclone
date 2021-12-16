fa06e62ae1437c8d6167fd2daad7f6ae19ffd4f9
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2021-12-16 23:02:44 +0600

    bug fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,19 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-  authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-  projectId: "disneyplus-clone-a33d5",
-  storageBucket: "disneyplus-clone-a33d5.appspot.com",
-  messagingSenderId: "37918794208",
-  appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-  measurementId: "G-DRVLJKWRWG",
+  apiKey: "AIzaSyDR5UDNeNLdOcXJ10sZh8oQMUlO4nW9vlc",
+  authDomain: "ahj-disney-clone.firebaseapp.com",
+  projectId: "ahj-disney-clone",
+  storageBucket: "ahj-disney-clone.appspot.com",
+  messagingSenderId: "415381924431",
+  appId: "1:415381924431:web:0a7896dd6cd1eee7d0583a",
+  measurementId: "G-QBZ7GJWV5C",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
-const storage = firebase.storage();
 
-export { auth, provider, storage };
+export { auth, provider };
 export default db;
